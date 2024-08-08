53853bd9343b68d6dacc1f162ba5809ec4ae0a14
Cinta jose <cjose@orthofx.com>
2024-08-08 11:31:15 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Cinta Jose on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
